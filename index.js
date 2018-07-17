3d540cfbb21d5d7161e8c9e7553942adfc5fea1c
Bhavuk Jain <bhavuk_92@yahoo.co.in>
2018-07-18 01:13:17 +0530

    Added image component replacer.

---
+++ index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import CacheManager from './CacheManager'
+
+export default class CustomImage extends React.Component {
+
+
+  state = {
+    isLoaded:false,
+    path:''
+  }
+
+  componentDidMount() {
+
+    const {source} = this.props
+
+    if(!source.uri) {
+      this.setState({path:source, isLoaded:true})
+      return
+    }
+
+    CacheManager.get(source.uri).getPath()
+    .then(path => {
+        this.setState({path:path, isLoaded:true})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  getImageComponent = () => {
+
+    const {source, ...otherProps} = this.props
+
+    if(this.state.isLoaded) {
+      if(source.uri) {
+        return (
+            <Image source={{uri:this.state.path}} {...otherProps}/>
+          )
+      }else {
+        return (
+            <Image source={this.props.source} {...otherProps}/>
+          )
+      }
+    }else {
+      if(this.props.placeholder) {
+        return (
+            <Image source={this.props.placeholder} {...otherProps}/>
+          )
+      }
+    }
+  }
+
+  render() {
+
+    return (
+      <View>
+        {this.getImageComponent()}
+      </View>
+    );
+  }
+}
